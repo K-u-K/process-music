396683e8ee34efb7b7a54defb69da11fc3d848df
rathalos64 <alen.kocaj@posteo.at>
2020-01-02 14:45:02 +0100

    Consider multiple tracks

--- process_music.py
+++ process_music.py
@@ -62,112 +62,138 @@ def get_note_type(ticks, ticks_per_beat):
 def get_note_number(note):
     note -= 21
     notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-    octave = note / 12 + 1
+    octave = int(note / 12) + 1
     name = notes[note % 12]
-    return f"{name}{str(int(octave))}"
+    return f"{name}{octave}"
 
-if __name__ == '__main__':
-    filename = sys.argv[1] if len(sys.argv) > 1 else ""
-    measures = sys.argv[2] if len(sys.argv) > 2 else 1
-    output   = sys.argv[3] if len(sys.argv) > 3 else "process_music.csv"
-
-    if not os.path.exists(filename):
-        print(f"file {filename} not found")
-        sys.exit(1)
-    
-    mid = MidiFile(filename, clip=True)
-
-    state   = {}
-    results = []
-
-    default = lambda: {"key": "", "msg": None}
-    prev    = default()
-    chord   = default()
-
-    order       = 1
-    case_number = 1
-    is_first    = True
-
-    time_signature = [*filter(lambda msg: msg.type == "time_signature", mid.tracks[0])]
-    if len(time_signature) == 0:
-        print(f"invalid midi file, not time signature message found")
-        sys.exit(1)
-    
-    numerator   = time_signature[0].numerator
-    denominator = time_signature[0].denominator
+def get_time_signature_ticks(time_signature, ticks_per_beat, measures):
+    numerator   = time_signature.numerator
+    denominator = time_signature.denominator
 
     if not denominator in SIGNATURE_UNIT.keys():
-        print(f"invlaid denominator / signature {time_signature[0]}")
+        print(f"invalid denominator / signature: {time_signature}")
         sys.exit(1)
 
     unit                     = SIGNATURE_UNIT[denominator]
     ratio_lower, ratio_upper = NOTE_TYPES[unit]
 
-    ticks_upper = mid.ticks_per_beat * ratio_upper * numerator * measures
-    ticks_lower = mid.ticks_per_beat * ratio_lower * numerator * measures
+    ticks_upper = ticks_per_beat * ratio_upper * numerator * measures
+    ticks_lower = ticks_per_beat * ratio_lower * numerator * measures
 
-    ticks = 0
-    for msg in mid.tracks[0]:
-        if msg.type not in ['note_on', 'note_off']:
+    return [ticks_lower, ticks_upper]
+
+def get_default_time_signature_ticks(ticks_per_beat, measures):
+    meta = mido.MetaMessage("time_signature", numerator=4, denominator=4)
+    return get_time_signature_ticks(meta, ticks_per_beat, measures)
+
+if __name__ == '__main__':
+    filename   = sys.argv[1] if len(sys.argv) > 1 else ""
+    measures   = sys.argv[2] if len(sys.argv) > 2 else 1
+    output_dir = sys.argv[3] if len(sys.argv) > 3 else ""
+
+    if not os.path.exists(filename):
+        print(f"file {filename} not found")
+        sys.exit(1)
+
+    if output_dir == "":
+        output_dir = os.path.splitext(filename)[0].lower()
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    mid = MidiFile(filename, clip=True)
+
+    for i, track in enumerate(mid.tracks):
+        state   = {}
+        results = []
+
+        default = lambda: {"key": "", "msg": None}
+        prev    = default()
+        chord   = default()
+
+        order       = 1
+        case_number = 1    
+        ticks       = 0
+        is_first    = True
+
+        # assume default time signature is 4/4
+        ticks_lower, ticks_upper = get_default_time_signature_ticks(mid.ticks_per_beat, measures)
+
+        # analyse meta tracks in terms of time_signature / set_tempo
+        if all([msg.is_meta for msg in track]):
+            time_signatures = [*filter(lambda msg: msg.type == "time_signature", track)]
+
+            # multiple time_signatures are uncommon but technically possible (take the last ticks)
+            for time_signature in time_signatures:
+                 ticks_lower, ticks_upper = get_time_signature_ticks(time_signature, mid.ticks_per_beat, measures)
+                 
             continue
 
-        ticks = ticks + msg.time
-        if ticks_lower <= ticks <= ticks_upper:
-            case_number = case_number + 1
-            ticks       = 0
+        # process main tracks
+        for msg in track:
+            if msg.type == "time_signature":
+                ticks_lower, ticks_upper = get_time_signature_ticks(msg, mid.ticks_per_beat, measures)
 
-        if msg.velocity == 0 or msg.type == 'note_off':
-            key       = get_note_number(msg.note)
-            note_type = get_note_type(msg.time, mid.ticks_per_beat)
+            if msg.type not in ['note_on', 'note_off']:
+                continue
 
-            time = msg.time
-            if state[key]["is_chord"]:
-                if time == 0:
-                    time = chord["msg"].time
-                else: 
-                    chord = {
-                        "key": key,
-                        "msg": msg
-                    }
-            
-            state[key]["type"] = get_note_type(time, mid.ticks_per_beat)
-            
-            results.append(state[key])
-            prev = default()
-            del state[key]
-            continue
+            ticks = ticks + msg.time
+            if ticks_lower <= ticks <= ticks_upper:
+                case_number = case_number + 1
+                ticks       = 0
 
-        is_chord = False
-        if prev["msg"] is not None and prev["msg"].type == "note_on" and msg.time == 0 and not is_first:
-            is_chord = True
-            state[prev["key"]]["is_chord"] = True
-            order = order - 1
+            if msg.velocity == 0 or msg.type == 'note_off':
+                key       = get_note_number(msg.note)
+                note_type = get_note_type(msg.time, mid.ticks_per_beat)
 
-        key = get_note_number(msg.note)
-        state[key] = {
-            "case":     case_number,
-            "key":      key,
-            "type":     "",
-            "order":    order,
-            "is_chord": is_chord,
-        }
+                time = msg.time
+                if state[key]["is_chord"]:
+                    if time == 0:
+                        time = chord["msg"].time
+                    else: 
+                        chord = {
+                            "key": key,
+                            "msg": msg
+                        }
+                
+                state[key]["type"] = get_note_type(time, mid.ticks_per_beat)
+                
+                results.append(state[key])
+                prev = default()
+                del state[key]
+                continue
 
-        prev = {
-            "key": key,
-            "msg": msg
-        }
-        order    = order + 1
-        is_first = False
+            is_chord = False
+            if prev["msg"] is not None and prev["msg"].type == "note_on" and msg.time == 0 and not is_first:
+                is_chord = True
+                state[prev["key"]]["is_chord"] = True
+                order = order - 1
 
-    with open(output, "w") as fh:
-        fh.write("Case_ID;Event;Type;Order;Is_Chord\n")
-        for result in results:
-            fh.write("{};{};{};{};{}\n".format(
-                result["case"],
-                result["key"],
-                result["type"],
-                result["order"],
-                result["is_chord"]
-            ))
+            key = get_note_number(msg.note)
+            state[key] = {
+                "case":     case_number,
+                "key":      key,
+                "type":     "",
+                "order":    order,
+                "is_chord": is_chord,
+            }
 
-    print(f"Logfile generated at {output}")
+            prev = {
+                "key": key,
+                "msg": msg
+            }
+            order    = order + 1
+            is_first = False
+
+        with open(f"{output_dir}/track_{i}.csv", "w") as fh:
+            fh.write("Case_ID;Event;Type;Order;Is_Chord\n")
+            for result in results:
+                fh.write("{};{};{};{};{}\n".format(
+                    result["case"],
+                    result["key"],
+                    result["type"],
+                    result["order"],
+                    result["is_chord"]
+                ))
+
+    print(f"Logfile generated at in directory '{output_dir}'")
