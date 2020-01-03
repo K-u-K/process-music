4895da90da3b3fdaf84e17ee5fe08b8783cf311f
rathalos64 <alen.kocaj@posteo.at>
2020-01-03 11:28:29 +0100

    Code refactoring

--- process_music/constants.py
+++ process_music/constants.py
@@ -2,7 +2,7 @@ import numpy as np
 
 NOTE_WHOLE                            = "whole note"
 NOTE_HALF                             = "half note"
-NOTE_QUARTER                          = "quater note"
+NOTE_QUARTER                          = "quarter note"
 NOTE_EIGHTH                           = "eighth note"
 NOTE_SIXTEENTH                        = "sixteenth note"
 NOTE_THIRTY_SECOND                    = "thirty-second note"
@@ -10,7 +10,7 @@ NOTE_SIXTY_FOURTH                     = "sixty-fourth note"
 NOTE_A_HUNDRED_AND_TWENTY_EIGHTH_NOTE = "a hundred and twenty-eighth note"
 
 SIGNATURE_UNIT = {
-    1:   NOTE_WHOLE
+    1:   NOTE_WHOLE,
     2:   NOTE_HALF,
     4:   NOTE_QUARTER,
     8:   NOTE_EIGHTH,
@@ -31,20 +31,24 @@ NOTES = [
     NOTE_A_HUNDRED_AND_TWENTY_EIGHTH_NOTE
 ]
 
-# base ratio between MIDI's ticks_per_beat (in quarter notes) and a whole note
-BASE_RATIO = 4.0
+def calculate_ratios(base_ratio, deviation, notes):
+    note_types = {}
+    base       = np.array([base_ratio - deviation, base_ratio + deviation])
 
+    for i, note in enumerate(notes):
+        bounds = (base * (0.5 ** i))
+
+        note_types[f"double dotted {note}"] = bounds * 1.75
+        note_types[f"dotted {note}"]        = bounds * 1.5
+        note_types[f"{note}"]               = bounds
+
+    return note_types
+
+# base ratio between MIDI's ticks_per_beat (in quarter notes) and a whole note
 # deviation is for things like MuseScore's strange tick numbering
+BASE_RATIO = 4.0
 DEVIATION  = 0.025
-BASE       = np.array([BASE_RATIO - DEVIATION, BASE_RATIO + DEVIATION])
 
 # calculate all ratios and include dotted + double dotted type notes
-NOTE_TYPES = {}
-for i, note in enumerate(NOTES):
-    bounds = (BASE * (0.5 ** i))
-
-    NOTE_TYPES[f"double dotted {note}"] = bounds * 1.75
-    NOTE_TYPES[f"dotted {note}"]        = bounds * 1.5
-    NOTE_TYPES[f"{note}"]               = bounds
-    
+NOTE_TYPES       = calculate_ratios(BASE_RATIO, DEVIATION, NOTES)
 NOTE_LOWER_BOUND = np.min(list(NOTE_TYPES.values()))
